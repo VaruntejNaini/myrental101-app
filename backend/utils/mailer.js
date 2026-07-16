96ee9c7c090cc3d5f776ad0d9ba1aaf34aec8f63
VaruntejNaini <varuncode7@gmail.com>
2026-07-16 14:04:37 +0530

    nodemailer fix

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,8 +1,6 @@
 import nodemailer from "nodemailer";
 
 import dns from "node:dns";
-import nodemailer from "nodemailer";
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,

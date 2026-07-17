1af7152df25d60027104e27252e260aa315c8b66
VaruntejNaini <varuncode7@gmail.com>
2026-07-17 20:26:36 +0530

    resend fix

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -23,10 +23,7 @@ const sendMail = async (mailOptions) => {
   console.log("To:", mailOptions.to);
   console.log("Subject:", mailOptions.subject);
   console.log("RESEND_API_KEY set:", !!process.env.RESEND_API_KEY);
-  console.log(
-  "RESEND_API_KEY prefix:",
-  process.env.RESEND_API_KEY?.substring(0, 10)
-);
+ console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 console.log(
   "RESEND_API_KEY length:",
   process.env.RESEND_API_KEY?.length
